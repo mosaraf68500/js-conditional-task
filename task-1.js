e9b751229b848b0308d5f50d975c2326687eb8a3
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2024-08-30 12:35:13 +0600

    task-1

---
+++ task-1.js
@@ -0,0 +1,21 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+
+let money=700;
+if(money>500){
+    console.log('Burger');
+}
+else{
+    if(money>30)
+    {
+        console.log('coke');
+    }
+    else{
+        console.log('free drinks');
+    }
+}
